4cc1c48b044267a1f0411bc77eab767292794cf1
Ziggy Chen <ziggycs9796@hotmail.com>
2023-10-07 14:54:29 +0800

    build: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,6 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   rules: {
-    'node/prefer-global/buffer': ['off', 'never'],
+    'node/prefer-global/buffer': ['off'],
   },
 })
